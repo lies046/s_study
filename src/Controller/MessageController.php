9b420d31185acea58ae015ae95b8ba3071b7bf24
Shohei Ito <skytree.skysky@gmail.com>
2021-12-02 11:51:54 +0900

    controller修正

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Conversation;
+use App\Entity\Message;
 use App\Repository\MessageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/messages', name: 'messages')]
 class MessageController extends AbstractController
 {
 
+    const ATTRIBUTE_TO_SERIALIZE = ['id', 'content', 'createdAt', 'mine'];
 
     /**
      * @var EntityManagerInterface
@@ -33,9 +37,9 @@ class MessageController extends AbstractController
     /**
      * @param Request $request
      * @param Conversation $conversation
-     * @return Response
+     * @return JsonResponse
      */
-    #[Route('/{id}', name: 'getMessage')]
+    #[Route('/{id}', name: 'getMessage', methods: 'GET')]
     public function index(Request $request, Conversation $conversation): Response
     {
         $this->denyAccessUnlessGranted('view', $conversation);
@@ -43,8 +47,55 @@ class MessageController extends AbstractController
             $conversation->getId()
         );
 
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+        /**
+         * @var $message Message
+         */
+        array_map(function ($message) {
+            $message->setMine(
+                $message->getUser()->getId() === $this->getUser()->getId()
+                    ? true : false
+            );
+        }, $messages);
+
+        return $this->json($messages, Response::HTTP_OK, [], [
+            'attributes' => self::ATTRIBUTE_TO_SERIALIZE
+        ]);
+    }
+
+
+    /**
+     * @param Request $request
+     * @param Conversation $conversation
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    #[Route('/{id}', name: 'newMessage', methods: 'POST')]
+    public function newMessage(Request $request, Conversation $conversation)
+    {
+        $user = $this->getUser();
+        $content = $request->get('content', null);
+
+        $message = new Message();
+        $message->setContent($content)
+            ->setUser($user)
+            ->setMine(true);
+        $conversation->addMessage($message)
+            ->setLastMessage($message);
+
+        $this->entityManager->getConnection()->beginTransaction();
+        try {
+            $this->entityManager->flush($message);
+            $this->entityManager->flush($conversation);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        }catch (\Exception $e){
+            $this->entityManager->rollback();
+            throw $e;
+        }
+
+
+        return $this->json($message, Response::HTTP_CREATED,[],[
+            'attributes' => self::ATTRIBUTE_TO_SERIALIZE
         ]);
     }
 }
